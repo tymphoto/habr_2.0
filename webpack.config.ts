757c0c0bd8a29b03f3fd959131c82bbd0c3a636e
Artem Timoshenko <atimoshenko@ubic.tech>
2022-09-13 13:06:07 +0300

    webpack + ts config

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from "path";
+import webpack from "webpack";
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+   mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  plugins: [
+    //подключение html файла в сборку
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }), 
+    // Отображение процесса сборки
+    new webpack.ProgressPlugin(), 
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+} 
+
+export default config 
