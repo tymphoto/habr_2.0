1a5ed64f64877e11958154e16666f054b9c6953f
Artem Timoshenko <atimoshenko@ubic.tech>
2022-09-13 17:38:02 +0300

    react + scss loader added

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,18 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+
+  const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      };
   
   const typescriptLoader = {
         test: /\.tsx?$/,
@@ -8,7 +20,9 @@ export function buildLoaders(): webpack.RuleSetRule[] {
         exclude: /node_modules/,
       };
 
+
   return [
       typescriptLoader,
+      cssLoader,
     ]
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -6,9 +6,9 @@ import { BuildEnv, BuildPaths } from "./config/build/types/config";
 export default (env: BuildEnv) => {
 
   const paths: BuildPaths = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  build: path.resolve(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
   }
 
   const mode = env.mode || 'development';
