3023a5a3194022f8db4e682817dbef5e4ee0e00e
tymphoto <9256187876@mail.ru>
2022-09-14 13:30:52 +0300

    scss variables, themes, ThemeContext, useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Suspense } from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
-import './index.scss'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { Suspense, useContext, useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import './styles/index.scss'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       
