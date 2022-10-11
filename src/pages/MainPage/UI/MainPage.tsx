a4743a347e776e37283aac045f5d13f840afbf15
Artem Timoshenko <atimoshenko@ubic.tech>
2022-10-11 16:05:02 +0300

    add LoginModal, input ui

--- src/pages/MainPage/UI/MainPage.tsx
+++ src/pages/MainPage/UI/MainPage.tsx
@@ -1,8 +1,16 @@
 import { Counter } from 'entities/Counter';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
+  const [value, setValue] = useState('');
+
+  const onChange = (val: string) => {
+    setValue(val);
+  };
+
   return (
     <div>
       {t('Главная страница')}
