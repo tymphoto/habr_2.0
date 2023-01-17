6f2cd031f876043f4b1b010abbe1cbe89117407f
Artem Timoshenko <atimoshenko@ubic.tech>
2023-01-17 17:12:19 +0300

    add Gray Theme, mobilepalette

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,7 +10,21 @@ export function useTheme():UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+    switch (theme) {
+    case Theme.DARK:
+      newTheme = Theme.LIGHT;
+      break;
+    case Theme.LIGHT:
+      newTheme = Theme.GRAY;
+      break;
+    case Theme.GRAY:
+      newTheme = Theme.DARK;
+      break;
+    default:
+      newTheme = Theme.LIGHT;
+    }
+
     setTheme?.(newTheme);
     document.body.className = newTheme;
     localStorage.setItem(LOCALSTORAGE_THEME_KEY, newTheme);
