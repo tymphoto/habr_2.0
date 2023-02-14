3d53d3aeebb677693982d50304dabd0c1344edf5
Artem Timoshenko <atimoshenko@ubic.tech>
2023-02-14 15:20:31 +0300

    @/pages/MainPage/UI/MainPage.tsx: delete unneccesery

--- src/pages/MainPage/UI/MainPage.tsx
+++ src/pages/MainPage/UI/MainPage.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ListBox } from 'shared/ui/ListBox/ListBox';
-import { HStack } from 'shared/ui/Stack';
 import { Page } from 'widgets/Page';
 
 const MainPage = () => {
@@ -15,24 +13,6 @@ const MainPage = () => {
   return (
     <Page>
       {t('Главная страница')}
-      {/* <div>adaslkmfsl</div>
-      <HStack>
-        <div>adaslkmfsl</div>
-        <ListBox
-          defaultValue="Выберите значение"
-          onChange={(value: string) => { }}
-          value={undefined}
-          items={[
-            { value: '1', content: '1' },
-            { value: '2', content: '2', disabled: true },
-            { value: '3', content: '3' },
-          ]}
-        />
-      </HStack>
-      <div>adaslkmfsl</div>
-      <div>adaslkmfsl</div>
-      <div>adaslkmfsl</div>
-      <div>adaslkmfsl</div> */}
     </Page>
   );
 };
