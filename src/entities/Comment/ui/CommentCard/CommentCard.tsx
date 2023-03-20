24f5b1c34df7cb599106f22eea1ecf217a56ca44
Artem Timoshenko <atimoshenko@ubic.tech>
2023-03-20 16:22:30 +0300

    fix styles problems

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -20,13 +20,13 @@ export const CommentCard = memo((props: CommentCardProps) => {
 
   if (isLoading) {
     return (
-      <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+      <VStack gap="8" max className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
         <div className={cls.header}>
           <Skeleton width={30} height={30} border="50%" />
           <Skeleton width={100} height={16} className={cls.username} />
         </div>
         <Skeleton width="100%" height={50} className={cls.text} />
-      </div>
+      </VStack>
     );
   }
 
--- src/entities/Comment/ui/CommentList/CommentList.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -18,7 +18,7 @@ export const CommentList = memo((props: CommentListProps) => {
 
   if (isLoading) {
     return (
-      <VStack gap="16" className={classNames('', {}, [className])}>
+      <VStack gap="16" max className={classNames('', {}, [className])}>
         <CommentCard isLoading />
         <CommentCard isLoading />
         <CommentCard isLoading />
