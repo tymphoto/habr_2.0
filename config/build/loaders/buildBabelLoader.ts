1d3820beee03977852c1e12f021f08062379f243
Artem Timoshenko <atimoshenko@ubic.tech>
2023-04-13 17:46:21 +0300

    upgrade webpack config, add babel cache

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -6,21 +6,16 @@ interface BuildBabelLoader extends BuildOptions {
 }
 
 export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoader) {
+  const isProd = !isDev;
   return {
     test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
     exclude: /node_modules/,
     use: {
       loader: 'babel-loader',
       options: {
+        cacheDirectory: true,
         presets: ['@babel/preset-env'],
         plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['ru', 'en'],
-              keyAsDefaultValue: true,
-            },
-          ],
           [
             '@babel/plugin-transform-typescript',
             {
@@ -28,7 +23,7 @@ export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoader) {
             },
           ],
           '@babel/plugin-transform-runtime',
-          isTsx && [
+          isTsx && isProd && [
             babelRemovePropsPlugin,
             {
               props: ['data-testid'],
