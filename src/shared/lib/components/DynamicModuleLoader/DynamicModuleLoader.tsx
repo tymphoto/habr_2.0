1aa9183d54fb9a0f412ace31b771ac94b6305769
Artem Timoshenko <atimoshenko@ubic.tech>
2023-04-13 16:47:35 +0300

    @/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx: fix ReducersList generic

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -1,10 +1,10 @@
 import { Reducer } from '@reduxjs/toolkit';
 import { ReactNode, useEffect } from 'react';
 import { useDispatch, useStore } from 'react-redux';
-import { ReduxStoreWithManager, StateSchemaKey } from '@/app/providers/StoreProvider';
+import { ReduxStoreWithManager, StateSchema, StateSchemaKey } from '@/app/providers/StoreProvider';
 
 export type ReducersList = {
-  [name in StateSchemaKey]?: Reducer;
+  [name in StateSchemaKey]?: Reducer<NonNullable<StateSchema[name]>>;
 }
 
 interface DynamicModuleLoaderProps {
