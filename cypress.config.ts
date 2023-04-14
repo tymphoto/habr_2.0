5eb1e4748fa90c5dd169f4ba2b9c4716df02c9e7
Artem Timoshenko <atimoshenko@ubic.tech>
2023-04-14 15:19:02 +0300

    isolated component cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
     },
     baseUrl: 'http://127.0.0.1:5173/',
   },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+    },
+  },
 });
