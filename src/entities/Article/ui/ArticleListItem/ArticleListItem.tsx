566a9955a4bfa5d6a9c4ce7f2e4083e60ba70541
Artem Timoshenko <atimoshenko@ubic.tech>
2023-04-14 06:34:02 +0300

    add AppImage, preload of images

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -16,6 +16,8 @@ import {
 import cls from './ArticleListItem.module.scss';
 import { ArticleTextBlockComponent } from '../ArticleTextComponent/ArticleTextBlockComponent';
 import { ArticleBlockType, ArticleView } from '../../model/consts/consts';
+import { AppImage } from '@/shared/ui/AppImage';
+import { Skeleton } from '@/shared/ui/Skeleton';
 
 interface ArticleListItemProps {
   className?: string;
@@ -56,7 +58,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
           </div>
           <Text title={article.title} className={cls.title} />
           {types}
-          <img alt={article.title} src={article.img} className={cls.img} />
+          <AppImage
+            fallback={<Skeleton width="100%" height="250px" />}
+            alt={article.title}
+            src={article.img}
+            className={cls.img}
+          />
           {textBlock && (
             <ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
           )}
@@ -86,7 +93,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
     >
       <Card className={cls.card}>
         <div className={cls.imageWrapper}>
-          <img src={article.img} alt={article.title} className={cls.image} />
+          <AppImage
+            fallback={<Skeleton width="200px" height="200px" />}
+            alt={article.title}
+            src={article.img}
+            className={cls.image}
+          />
           <Text text={article.createdAt} className={cls.date} />
         </div>
         <div className={cls.infoWrapper}>
--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -48,7 +48,7 @@ export const AvatarDropdown = memo((props: AvatarDropdownProps) => {
           onClick: onLogout,
         },
       ]}
-      trigger={<Avatar size={30} src={authData.avatar} />}
+      trigger={<Avatar fallbackInverted size={30} src={authData.avatar} />}
       direction="bottom left"
     />
   );
