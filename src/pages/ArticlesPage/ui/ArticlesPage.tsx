e392c1554175379eee743ebb42163a8150fb38c5
Artem Timoshenko <atimoshenko@ubic.tech>
2023-06-20 16:35:44 +0300

    add greeting modal for articles page

--- src/pages/ArticlesPage/ui/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage.tsx
@@ -8,6 +8,7 @@ import { articlesPageReducer } from '../model/slices/articlePageSlice';
 import { fetchNextArticlePage } from '../model/services/fetchNextArticlePage/fetchNextArticlePage';
 import { ArticlesPageFilters } from './ArticlesPageFilters/ArticlesPageFilters';
 import { ArticleInfiniteList } from './ArticleInfiniteList/ArticleInfiniteList';
+import { ArticlePageGreeting } from '@/features/articlePageGreeting';
 
 interface ArticlesPageProps {
   className?: string;
@@ -34,6 +35,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
       >
         <ArticlesPageFilters />
         <ArticleInfiniteList className={cls.list} />
+        <ArticlePageGreeting />
       </Page>
     </DynamicModuleLoader>
   );
