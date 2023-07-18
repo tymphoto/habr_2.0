4c627d511d676d703135b962c7a59827d3774fcf
Artem Timoshenko <atimoshenko@ubic.tech>
2023-07-18 09:48:28 +0300

    scripts/remove-feature.ts: add return

--- scripts/remove-feature.ts
+++ scripts/remove-feature.ts
@@ -125,16 +125,17 @@ const replaceComponent = (node: Node) => {
 };
 
 files.forEach((sourceFile) => {
+    // eslint-disable-next-line consistent-return
     sourceFile.forEachDescendant((node) => {
         if (node.isKind(SyntaxKind.CallExpression) && isToggleFunction(node)) {
-            replaceToggleFunction(node);
+            return replaceToggleFunction(node);
         }
 
         if (
             node.isKind(SyntaxKind.JsxSelfClosingElement) &&
             isToggleComponent(node)
         ) {
-            replaceComponent(node);
+            return replaceComponent(node);
         }
     });
 });
