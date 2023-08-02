d7e672d02de9551280adcd18993ba07ee63d506c
Artem Timoshenko <atimoshenko@ubic.tech>
2023-08-02 08:55:04 +0300

    @/pages/MainPage/index.ts: update public api

--- src/pages/MainPage/index.ts
+++ src/pages/MainPage/index.ts
@@ -1,5 +1,7 @@
 import { MainPageAsync } from './UI/MainPage.async';
+import { SecondTestPageAsync } from './UI/SecondTestPage.async'
 
 export {
   MainPageAsync as MainPage,
+  SecondTestPageAsync as SecondTestPage,
 };
